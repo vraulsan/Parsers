912453a04880f2617aebfaf9e7612ac3fc6836ad
Victor <vraulsan@gmail.com>
2016-07-03 13:51:04 -0400

    Update pulling_data_mysql.py

--- pulling_data_mysql.py
+++ pulling_data_mysql.py
@@ -35,68 +35,41 @@ class Database:
 if __name__ == "__main__":
     db = Database()
 
-# define function for when searching by City
-def bycity():
-    what_city = raw_input("Search by city: ")
-    q = ("SELECT * FROM  `customers` WHERE `City` REGEXP \'%s\' LIMIT 0 , 30" % what_city)
-    search = db.query(str(q))
-    for entry in search:
-        print "Found: %s" % entry['Name'],
-        print "in %s" % entry['City'], 
-        print "with Mgmt IP of %s" % entry['Mgmt IP']
+# ask the user what they want to search by
+bywhat = input("""
+What do you want to search by:
+	1. Search by Name
+	2. Search by Address
+	3. Search by City
+	4. Search by Vendor
+	""")
+# transform the user input to the proper string to pass to the query db.query function
+if bywhat == 1:
+	bywhat = "Name"
+elif bywhat == 2:
+	bywhat = "Address"
+elif bywhat == 3:
+	bywhat = "City"
+elif bywhat == 4:
+	bywhat = "Vendor"
+else: 
+	print "Wrong input, try again."
+	exit()
 
+# ask the user what is the value of their search method
+what = raw_input("Search by %s: " % bywhat)
 
-# define function for when searching by Name
-def byname():
-    what_name = raw_input("Search by name: ")
-    q = ("SELECT * FROM  `customers` WHERE `Name` REGEXP \'%s\' LIMIT 0 , 30" % what_name)
-    search = db.query(str(q))
-    for entry in search:
-        print "Found: %s" % entry['Name'],
-        print "in %s" % entry['City'], 
-        print "with Mgmt IP of %s" % entry['Mgmt IP']
+# define the query string
+q = ("SELECT * FROM  `customers` WHERE `%s` REGEXP \'%s\' LIMIT 0 , 30" % (bywhat, what))
+	#return q
 
+# store the query output in variable named search
+search = db.query(str(q))
 
-# define function for when searching by Name
-def byaddress():
-    what_address = raw_input("Search by address: ")
-    q = ("SELECT * FROM  `customers` WHERE `Address` REGEXP \'%s\' LIMIT 0 , 30" % what_address)
-    search = db.query(str(q))
-    for entry in search:
-        print "Found: %s" % entry['Name'],
-        print "at %s" % entry['Address'], 
-        print "with Mgmt IP of %s" % entry['Mgmt IP']
-
-
-# define function for when searching by Vendor
-def byvendor():
-    what_vendor = raw_input("Search by Vendor: ")
-    q = ("SELECT * FROM  `customers` WHERE `Vendor` REGEXP \'%s\' LIMIT 0 , 30" % what_vendor)
-    search = db.query(str(q))
-    for entry in search:
+# use for loop to print out values for each position of the table
+for entry in search:
         print "Found: %s" % entry['Name'],
         print "at %s" % entry['Address'], 
         print "using %s" % entry['Vendor'],
+        print "in %s" % entry['City'],
         print "with Mgmt IP of %s" % entry['Mgmt IP']
-
-# display Menu with options for searching
-msg = """
-    Please enter the number for your desired search:
-        1.  Search by Name
-        2.  Search by Address
-        3.  Search by City
-        4.  Search by Vendor
-    """
-option = input(msg)
-while option != 1 and option != 2 and option != 3 and option != 4:
-    option = input(msg)
-
-# call the functions
-if option == 1:
-    byname()
-if option == 2:
-    byaddress()
-if option == 3:
-    bycity()
-if option == 4:
-    byvendor()
