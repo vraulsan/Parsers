ad17e841e42ccde047182b281c887193a225a412
Victor <vraulsan@gmail.com>
2016-07-04 12:06:58 -0400

    Update pulling_data_mysql.py

--- pulling_data_mysql.py
+++ pulling_data_mysql.py
@@ -4,13 +4,14 @@
 #############################################################################################
 
 import MySQLdb
+import re
 
 ######################### Lets build our Class ################################
 
 class Database:
-    host = "hostname/ip"
+    host = "52.20.131.2"
     user = "ubuntu-vm"
-    passwd = "pass"
+    passwd = "kornkid182"
     db = "fireprotocol"
     
     # build the constructor
@@ -27,6 +28,13 @@ class Database:
         cursor.execute(q)
         # return all results from the query
         return cursor.fetchall()
+
+    #  define the query function for fetch one (not using right now)
+	def queryone(self, q):
+		cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute(q)
+		return cursor.fetchone()
+
     # and then close the connection with this other function
     def __del__(self):
         self.connection.close()
@@ -59,19 +67,20 @@ else:
 # ask the user what is the value of their search method
 what = raw_input("Search by %s: " % bywhat)
 
-# define the query string
+# define the query string for all rows all data
 q = ("SELECT * FROM  `customers` WHERE `%s` REGEXP \'%s\' LIMIT 0 , 30" % (bywhat, what))
-	#return q
+# define the query string for all rows Mgmt IP
+qIP = ("SELECT `Mgmt IP` FROM  `customers` WHERE `%s` REGEXP \'%s\' LIMIT 0 , 30" % (bywhat, what))
 
-# store the query output in variable named search
+# query the DB using q string
 search = db.query(str(q))
 
-i = 0
-# use for loop to print out values for each position of the table
+# use for loop to iterate through values for each position of the table
+e = 0
 for entry in search:
         print "----------------------------------------------------"
-        i += 1
-        print "%d. " % i,
+        e += 1
+        print "%d. " % e,
         print "%s" % entry['Name'],
         print "\n\tAddress: %s" % entry['Address'], 
         print "\n\tVendor: %s" % entry['Vendor'],
@@ -79,3 +88,29 @@ for entry in search:
         print "\n\tMgmt IP: %s" % entry['Mgmt IP']
         print "----------------------------------------------------"
 
+######################### Figure out IP to connect to #############################
+# query the DB using qIP string and then conver to a list
+search_IP = list(db.query(str(qIP)))
+# ask the user what box they want to get output from
+i = input("\nIf you would like to get an output from the box, enter the number for that box now :")
+# use this i variable to figure out the right list position 
+i = i - 1                                                                        
+# use variable i to extract the list position, meaning the desired IP
+myip = search_IP[i]                   
+# function to extract the IP number only and get it as a string
+def get_num(x):
+	return str(''.join(ele for ele in x if ele.isdigit() or ele == '.'))
+# transform list position into a string so we can put pass it to the get_num function
+myip = str(myip)
+myip = get_num(myip)
+print "The IP we are going to connect to is %s" % myip
+######################### Figure out IP to connect to #############################
+
+option = input("""
+	\t1. Running configuration
+	\t2. Interfaces status
+	\t3. IP addressing interface information
+	\t4. OSPF Neighbors summary
+	\t5. Logging
+	\t6. Version/Uptime information
+	Please enter an option from above: """)
